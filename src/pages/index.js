66e63834ccff72a9b7455b4db47a4ffa9bb84a20
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-04-18 07:33:26 +0600

    added tailwind configuration for it

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
-export default function Home(){
- return(<>
- <div>
-  Hello World;
- </div>
-  
-  </>)
+export default function Home() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center text-3xl text-blue-900 font-extrabold h-screen w-auto bg-red-200">Hello World from tailwind;</div>
+    </>
+  );
 }
